1516ea8a1e3ce644c7782d88c28e3dcba749b950
vasheev <vasheev@users.noreply.github.com>
2021-05-13 21:16:58 +0300

    Delete createDirFromList.ps1

--- createDirFromList.ps1
+++
@@ -1,6 +0,0 @@
-$keyword = Get-Content "C:\e\s4\keywords.txt"
-$keyword
-foreach($fold in $keyword){
-    $folder = "C:\e\s4\ноутбук\ключевые слова\" + $fold
-    New-Item -Path $folder -ItemType 'Directory'
-}
